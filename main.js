510000f528565aaabea212f888a5528bb4f4d02b
Brandon Chau <bchau03@gmail.com>
2024-12-18 13:49:37 -0800

    used flask to get scraped data

--- main.js
+++ main.js
@@ -3,20 +3,64 @@ window.onload = function(){
     const savedData = sessionStorage.getItem('bookData');
     if(savedData){
         const data = JSON.parse(savedData);
-        displayData(data);
+        displaySearch(data);
     }
 }
+
+document.addEventListener('DOMContentLoaded',function(){
+    fetch('books.json')
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+            //Displays 5 books in the div with id name 'hardcover_fiction' using handlebars template
+            displayBestSellers(document.getElementById('hardcover_fiction'), data.slice(0,5));
+            displayBestSellers(document.getElementById('hardcover_nonfiction'), data.slice(5,10));
+            displayBestSellers(document.getElementById('paperback_fiction'), data.slice(10,15));
+            displayBestSellers(document.getElementById('paperback_nonfiction'), data.slice(15,20));
+            displayBestSellers(document.getElementById('advice'), data.slice(20,25));
+            displayBestSellers(document.getElementById('hardcover_children'), data.slice(25,30));
+            displayBestSellers(document.getElementById('children_picture'), data.slice(30,35));
+            displayBestSellers(document.getElementById('children_YA'), data.slice(35,40));
+            displayBestSellers(document.getElementById('hardcover_YA'), data.slice(40,45));
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    }); 
+})
+
 // JSON DATA REFERENCE LINK: https://www.googleapis.com/books/v1/volumes?q=harrypotter
+
+//Displays NYT bestsellers
+// document.addEventListener('DOMContentLoaded', function(){
+//     $.ajax({
+//         url: 'http://127.0.0.1:5000/bestsellers',
+//         dataType: 'json',
+//         type: 'GET',
+
+//         success: function(data){
+//             //Displays 5 books in the div with id name 'hardcover_fiction' using handlebars template
+//             displayBestSellers(document.getElementById('hardcover_fiction'), data.slice(0,5));
+//             displayBestSellers(document.getElementById('hardcover_nonfiction'), data.slice(5,10));
+//             displayBestSellers(document.getElementById('paperback_fiction'), data.slice(10,15));
+//             displayBestSellers(document.getElementById('paperback_nonfiction'), data.slice(15,20));
+//             displayBestSellers(document.getElementById('advice'), data.slice(20,25));
+//             displayBestSellers(document.getElementById('hardcover_children'), data.slice(25,30));
+//             displayBestSellers(document.getElementById('children_picture'), data.slice(30,35));
+//             displayBestSellers(document.getElementById('children_YA'), data.slice(35,40));
+//             displayBestSellers(document.getElementById('hardcover_YA'), data.slice(40,45));
+//         }
+//     });
+// })
 
 function bookSearch(event){
     if(event.key == 'Enter'){
         var search = document.getElementById('search').value
         document.getElementById('results').innerHTML="";
-
-        var top100 = document.getElementById('top100');
         var search_results = document.getElementById('search-results');
-
-        console.log(search);
         //conducts the ajax request if theres something in the search bar
         if(search.trim() !== ''){
             $.ajax({
@@ -30,7 +74,7 @@ function bookSearch(event){
                     //hides the best sellers display and replaces it with search results
                     topsellers.style.display = 'none';
                     search_results.style.display = 'flex';
-                    displayData(data);
+                    displaySearch(data);
                 },
             });
         }
@@ -41,15 +85,25 @@ function bookSearch(event){
     }
 }
 
-function displayData(data){
-    var template = document.getElementById("template").innerHTML;
+//Uses "search-bestsellers" template to display the best sellers from the New York Times.
+function displayBestSellers(container, data){
+    var template = document.getElementById('search-bestsellers').innerHTML;
+    var compiledTemplate = Handlebars.compile(template);
+    var ourHTML = compiledTemplate(data);
+    container.innerHTML += ourHTML;
+}
+
+//Uses "search-template" to display the search results
+function displaySearch(data){
+    var template = document.getElementById("search-template").innerHTML;
     var compiledTemplate = Handlebars.compile(template);
     var ourHTML = compiledTemplate(data);
     var results = document.getElementById("results");
     results.innerHTML = ourHTML;
 }
 
-//Helper functions allows you to use functions inside the handlebars script, otherwise everything inside has to be html or handlebars
+//Helper functions allows you to use functions inside the handlebars script in the html file, otherwise everything inside has to be html or handlebars
+//Generates the link to search by title
 Handlebars.registerHelper('titleLink', 
     function(title, id) {
         //replace spaces in the book's title with underscores
@@ -59,11 +113,11 @@ Handlebars.registerHelper('titleLink',
         return link;
 });
 
+//Generates the link to search by author
 Handlebars.registerHelper('authorLink', 
     function(author) {
         var link = "https://www.google.com/search?sca_esv=280a44b9c274f648&udm=36&udm=36&q=inauthor:\"" + author + "\"";
         return link;
 });
 
-// document.getElementById('button').addEventListener('click', bookSearch, false)
 document.getElementById('search').addEventListener('keypress', bookSearch, false)
--- scraper.py
+++ scraper.py
@@ -1,31 +1,64 @@
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 #finds titles, authors, and descriptions based on html tags
 #returns a list of books
 def scrape_nyt():
-    url = 'https://www.nytimes.com/books/best-sellers/2024/11/24/'
+    url = "https://www.nytimes.com/books/best-sellers/"
+    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
     response = requests.get(url)
+
+    # print(response.status_code)
+    # print(response.headers)
+    # print(response.text[:1000])  # Print the first 1000 characters of the response body
+
     if response.status_code == 200:
         parsed = BeautifulSoup(response.text, 'html.parser')
 
-        authors, titles, descs, books = [], [], [], []
+        authors, titles, descs, books, images = [], [], [], [], []
         find_titles = parsed.find_all('h3', itemprop='name')
         find_authors = parsed.find_all('p', itemprop='author')
         find_descs = parsed.find_all('p', itemprop='description')
+        find_images = parsed.find_all('img', itemprop='image')
         for entry in find_titles:
             titles.append(entry.text)
         for entry in find_authors:
             authors.append((entry.text).replace('by ', ''))
         for entry in find_descs:
             descs.append(entry.text)
+        for entry in find_images:
+            images.append(entry['src'])
         for book in range(len(titles)):
             books.append({
                 'title': titles[book],
                 'author': authors[book],
-                'description': descs[book]
+                'description': descs[book],
+                'images': images[book]
             })
+        books = books[10:]
         return books
     else:
-        print("failed to get text.")
+        print("Failed to retrieve data. Status code:", response.status_code)
+
+app = Flask(__name__)
+CORS(app)
+bestsellers = []
+
+
+@app.route('/update_bestsellers')
+def update_bestsellers():
+    global bestsellers
+    bestsellers = scrape_nyt()
+    return jsonify({'message': "Data updated"})
+
+@app.route('/bestsellers')
+def get_bestsellers():
+    return jsonify(bestsellers)
+
+if __name__ == '__main__':
+    bestsellers = scrape_nyt()
+    app.run(debug=True)
+
+    
